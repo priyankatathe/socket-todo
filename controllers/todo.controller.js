1475eb59d2e8d499fb93d97e2e77d17d0e06813d
priya <priyankatathe4@gmail.com>
2024-08-27 12:53:51 +0530

    update controller

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,14 +1,19 @@
 const asyncHandler = require("express-async-handler")
+const Todo = require("../models/Todo")
 
 exports.createTodo = asyncHandler(async (req, res) => {
+    await Todo.create(req.body)
     res.json({ message: "createTodo successs" })
 })
 exports.readTodo = asyncHandler(async (req, res) => {
-    res.json({ message: "readcreateTodo successs" })
+    const result = await Todo.find()
+    res.json({ message: "readcreateTodo successs", result })
 })
 exports.updateTodo = asyncHandler(async (req, res) => {
+    await Todo.findByIdAndUpdate(req.params.id, req.body)
     res.json({ message: "updateTodo successs" })
 })
 exports.deleteTodo = asyncHandler(async (req, res) => {
+    await Todo.findByIdAndDelete(req.params.id)
     res.json({ message: "deleteTodo successs" })
 })
